f0f259ba60bc29e23aed3f40aac4cb11a4ff92ef
Pranaya20-30 <btechstudent.741@gmail.com>
2025-06-16 22:03:53 +0530

    Update CozyHevenApplication.java

--- czy_heven/coding/java/backend/src/main/com/hexaware/cozy_heven/CozyHevenApplication.java
+++ czy_heven/coding/java/backend/src/main/com/hexaware/cozy_heven/CozyHevenApplication.java
@@ -1,4 +1,4 @@
-package com.example.cozy_heven;
+package com.hexaware.cozy_heven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
