8fc00b85d1d7505202c4397ed76802f78546f720
Pranaya20-30 <btechstudent.741@gmail.com>
2025-06-16 22:20:38 +0530

    Delete czy_heven/coding/java/backend/src/main/com/hexaware/cozy_heven directory

--- czy_heven/coding/java/backend/src/main/com/hexaware/cozy_heven/Entity/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.cozy_heven.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.cozy_heven.entity.Users;
-
-public interface UserRepo extends JpaRepository<Users, Integer> {
-	Users findByEmail(String email);// You can add custom queries if needed
-}
