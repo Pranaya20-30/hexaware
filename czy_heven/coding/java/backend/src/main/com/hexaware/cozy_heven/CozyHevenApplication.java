87142a78096d58dc8278e9df1c2700d0acfbdf30
Pranaya20-30 <btechstudent.741@gmail.com>
2025-06-16 22:01:18 +0530

    Create CozyHevenApplication.java

---
+++ czy_heven/coding/java/backend/src/main/com/hexaware/cozy_heven/CozyHevenApplication.java
@@ -0,0 +1,13 @@
+package com.example.cozy_heven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyHevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyHevenApplication.class, args);
+	}
+
+}
